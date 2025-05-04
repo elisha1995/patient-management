8616c9e5a496280a41d8b80e9b6741260e42fb43
elisha1995 <elishasamuel01@gmail.com>
2025-05-04 15:52:30 +0000

    feat: add JwtValidationException and JwtValidationGatewayFilterFactory
    - Add JwtValidationException to handle unauthorized exceptions
    - Update application.yml to include JwtValidation filter
    - Implement JwtValidationGatewayFilterFactory for JWT validation

---
+++ api-gateway/src/main/java/com/gesacademy/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,46 @@
+package com.gesacademy.apigateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object>{
+
+    private final WebClient webClient;
+
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
+                                             @Value("${auth.service.url}") String authServiceUrl) {
+        this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+    }
+
+    /**
+     * This method returns a GatewayFilter that validates the JWT in the Authorization header by
+     * making a call to the Auth service. If the JWT is invalid, the filter sets the status code to
+     * UNAUTHORIZED and completes the response. If the JWT is valid, the filter continues the chain.
+     *
+     * @param config The configuration object for the filter. In this case, the config is ignored.
+     * @return A GatewayFilter that validates the JWT in the Authorization header.
+     */
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token =
+                    exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if(token == null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            return webClient.get()
+                    .uri("/validate")
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
+        };
+    }
+}
