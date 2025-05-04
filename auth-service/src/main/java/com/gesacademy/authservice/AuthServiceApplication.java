491341dd782f5bd511204a5d6488d08bc0db3bc5
elisha1995 <elishasamuel01@gmail.com>
2025-05-04 20:42:46 +0000

    feat(api-gateway): add OpenAPI documentation and routes for services
    - Add `springdoc-openapi-starter-webflux-ui` dependency to api-gateway's pom.xml
    - Update AuthServiceApplication and PatientServiceApplication with @OpenAPIDefinition annotations
    - Modify API Gateway routes to serve OpenAPI specs from patient-service and auth-service
    - Configure Swagger UI to display multiple service APIs

--- auth-service/src/main/java/com/gesacademy/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/gesacademy/authservice/AuthServiceApplication.java
@@ -1,9 +1,16 @@
 package com.gesacademy.authservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://localhost:4004/auth", description = "API Gateway")
+		}
+)
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
--- patient-service/src/main/java/com/gesacademy/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/gesacademy/patientservice/PatientServiceApplication.java
@@ -1,9 +1,16 @@
 package com.gesacademy.patientservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:4004/api/patients", description = "API Gateway")
+        }
+)
 public class PatientServiceApplication {
 
     public static void main(String[] args) {
