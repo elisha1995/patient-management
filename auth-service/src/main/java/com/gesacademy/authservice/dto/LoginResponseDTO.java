c5cb4f9533dd4ecff18176be0fbc5d05ae4f9860
elisha1995 <elishasamuel01@gmail.com>
2025-05-04 09:09:12 +0000

    Add login request and response DTOs
    Introduce `LoginRequestDTO` for handling login requests with validation annotations, and `LoginResponseDTO` to encapsulate the response token.

---
+++ auth-service/src/main/java/com/gesacademy/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.gesacademy.authservice.dto;
+
+public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
