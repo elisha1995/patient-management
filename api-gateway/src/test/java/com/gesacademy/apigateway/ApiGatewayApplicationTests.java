f4b9534ad4d08412ba3c14633e3ea76f6f1971cf
elisha1995 <elishasamuel01@gmail.com>
2025-05-04 07:28:35 +0000

    Add initial setup for API Gateway
    This commit adds the POM file and main application class to set up a basic Spring Boot API Gateway project.

---
+++ api-gateway/src/test/java/com/gesacademy/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesacademy.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
